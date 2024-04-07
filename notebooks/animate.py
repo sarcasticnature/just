0a1b18d349a695e37daa8d13ca26526ad87af382
Jake Keller <jakeller3630@gmail.com>
2024-04-06 19:14:17 -0500

    Clean up animation script and log file name

--- notebooks/animate.py
+++ notebooks/animate.py
@@ -1,36 +1,59 @@
 #!/bin/env python3
+"""Script for creating an animation from a JUST VFH agent log file."""
+
+import argparse
+
 from matplotlib import animation
 import matplotlib.pyplot as plt
 import numpy as np
 import h5py
 
-f = h5py.File('/tmp/just/jerry/foo.h5', 'r')
+# pylint: disable=no-member
 
-dset1 = f['vfh_agent/polar_histogram']
-dset2 = np.array(f['vfh_agent/window_histogram'])
-theta = np.linspace(0.0, 2 * np.pi, dset1.shape[0], endpoint=False)
+def main():
+    """The main fn."""
+    parser = argparse.ArgumentParser(
+        description='Create an animation of the window & polar histograms from a JUST log file',
+    )
+    parser.add_argument('-a', '--agent-name', default='jerry')
+    parser.add_argument('--fps', default=100, type=int)
 
-window_size = 30
+    args = parser.parse_args()
 
-fig = plt.figure()
-axes = [plt.subplot(1, 2, 1, projection='polar'), plt.subplot(1, 2, 2)]
+    with h5py.File(f'/tmp/just/{args.agent_name}/log.h5', 'r') as file:  # TODO: parameterize
 
-bars = axes[0].bar(theta, dset1[:,0])
-axes[0].set_ylim(0,dset1[:,:].max())
-window = axes[1].imshow(dset2[:,0].reshape(window_size,window_size), interpolation='none', origin='lower')
+        dset1 = file['vfh_agent/polar_histogram']
+        dset2 = np.array(file['vfh_agent/window_histogram'])
+        theta = np.linspace(0.0, 2 * np.pi, dset1.shape[0], endpoint=False)
 
-def init():
-    return bars, window
+        window_size = 30
 
-def animate(i):
-    for j, b in enumerate(bars):
-        b.set_height(dset1[j,i])
-    #axes[0].set_ylim(0,dset1[:,i].max())
-    foo = dset2[:,i].reshape(window_size, window_size)
-    foo[window_size // 2 - 1, window_size // 2 - 1] = 50
-    window.set_data(foo)
-    return bars, window
+        fig = plt.figure()
+        axes = [plt.subplot(1, 2, 1, projection='polar'), plt.subplot(1, 2, 2)]
 
-anim = animation.FuncAnimation(fig, animate, init_func=init,
-                               frames=dset1.shape[1], interval=10, blit=False)
-anim.save('basic_animation.mp4', writer=animation.FFMpegWriter(fps=100))
+        bars = axes[0].bar(theta, dset1[:,0])
+        axes[0].set_ylim(0,dset1[:,:].max())
+        window = axes[1].imshow(
+            dset2[:,0].reshape(window_size,window_size),
+            interpolation='none',
+            origin='lower',
+        )
+
+        def init():
+            return bars, window
+
+        def animate(i):
+            for j, bar in enumerate(bars):      # pylint: disable=disallowed-name
+                bar.set_height(dset1[j,i])
+            data = dset2[:,i].reshape(window_size, window_size)
+            data[window_size // 2 - 1, window_size // 2 - 1] = 50
+            window.set_data(data)
+            return bars, window
+
+        interval = 1000 // args.fps
+        anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                       frames=dset1.shape[1], interval=interval, blit=False)
+        anim.save('just_animation.mp4', writer=animation.FFMpegWriter(fps=args.fps))
+
+if __name__ == '__main__':
+    main()
